efbf561c58ddedd4fef707fc06f9b5f894c8a0ab
Brandan Haertel <foilpudding@gmail.com>
2016-06-26 13:47:02 -0700

    added health bar art and fixed the menu so clicks work. removed the controller support

---
+++ Assets/Resources/Scripts/GlobalVars.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GlobalVars : MonoBehaviour {
+
+	#region ENUMS
+	public enum GAME_SCENES {Menu = 0, game = 1};
+	#endregion
+	
+	private static GAME_SCENES _currentGameScene;
+	public static GAME_SCENES CurrentGameScene
+	{
+		get{
+			return _currentGameScene;
+		}
+		set
+		{
+			_currentGameScene = value;
+			Application.LoadLevel(value.ToString());
+		}
+	}
+}
---
+++ Assets/SimpleSceneStart.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleSceneStart : MonoBehaviour {	
+	public void OnPlayButtonClick()
+	{
+		print ("OnPlayButtonClick");
+		GlobalVars.CurrentGameScene = GlobalVars.GAME_SCENES.game;
+	}
+}
